8f27b97ed218c2869396953d60d58434c8e9c43e
muneesbeeone <127736059+muneesbeeone@users.noreply.github.com>
2025-09-24 19:09:00 +0530

    updated

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,18 +3,20 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
 
-  // Host on static hosting (GitHub Pages)
-  ssr: false,
-  nitro: { preset: 'github_pages' },
-
-  // Using a custom domain at the root, keep baseURL as '/'
-  app: { baseURL: '/' },
-
   modules: [
     '@nuxt/fonts',
     '@nuxt/icon',
     '@nuxt/image',
     '@nuxt/scripts',
     '@nuxtjs/tailwindcss'
-  ]
+  ],
+
+  scripts: {
+    // @ts-expect-error: 'crossorigin' is a valid attribute
+    'adsense-script': {
+      src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4391323106927085',
+      crossorigin: 'anonymous',
+      strategy: 'afterInteractive'
+    }
+  }
 })
